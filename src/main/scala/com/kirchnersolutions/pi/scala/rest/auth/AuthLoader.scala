5b87fb0cdf113f2bffc044da56990cf0a14efe85
Robert Kirchner JR <rjojjr91@live.com>
2019-12-05 15:42:04 -0600

    add config values to ConfigValues

--- src/main/scala/com/kirchnersolutions/pi/scala/rest/auth/AuthLoader.scala
+++ src/main/scala/com/kirchnersolutions/pi/scala/rest/auth/AuthLoader.scala
@@ -3,15 +3,15 @@ package com.kirchnersolutions.pi.scala.rest.auth
 import java.io.File
 import java.util.Base64
 
-import com.kirchnersolutions.utilities.ByteTools
+//import com.kirchnersolutions.utilities.ByteTools
 
 object AuthLoader {
 
   val path = "auth/auth.key"
   var key: String = null
 
-  def load = {
-    key = new String(ByteTools.readBytesFromFile(new File(path)), "UTF-8")
-  }
+  /*def load = {
+    key = new String(ByteTools readBytesFromFile(new File(path)), "UTF-8")
+  }*/
 
 }
