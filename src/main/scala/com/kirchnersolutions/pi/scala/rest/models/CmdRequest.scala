92b1041464d5b2ce9849b5b437ae76bf65ba4b07
Robert Kirchner JR <rjojjr91@live.com>
2019-12-06 19:45:53 -0600

    add CommandResponse

--- src/main/scala/com/kirchnersolutions/pi/scala/rest/models/CmdRequest.scala
+++
@@ -1,2 +0,0 @@
-package com.kirchnersolutions.pi.scala.rest.models
-case class CmdRequest(command: String)
---
+++ src/main/scala/com/kirchnersolutions/pi/scala/rest/models/CommandResponse.scala
@@ -0,0 +1,6 @@
+package com.kirchnersolutions.pi.scala.rest.models
+
+import com.kirchnersolutions.pi.scala.rest.traits.RestObject
+
+case class CommandResponse(command: String)
+object CommandResponse extends RestObject
