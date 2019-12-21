31031255d364774f44d1476272733a705ade0cd1
Robert Kirchner JR <rjojjr91@live.com>
2019-12-21 12:20:44 -0600

    add getProcesses and getPID to ProcessService

---
+++ src/main/scala/com/kirchnersolutions/pi/scala/rest/models/PsLine.scala
@@ -0,0 +1,14 @@
+package com.kirchnersolutions.pi.scala.rest.models
+
+case class PsLine(user: String,
+                  pid: Int,
+                  cpuTimeMonitoring: Double,
+                  mem: Double,
+                  vsx: Int,
+                  rss: Int,
+                  tty: String,
+                  stat: String,
+                  start: String,
+                  time: String,
+                  command: String)
+object PsLine
--- src/main/scala/com/kirchnersolutions/pi/scala/rest/services/ProcessService.scala
+++ src/main/scala/com/kirchnersolutions/pi/scala/rest/services/ProcessService.scala
@@ -3,6 +3,9 @@ package com.kirchnersolutions.pi.scala.rest.services
 import java.io.{BufferedReader, InputStreamReader}
 import java.lang.Process
 import java.lang.ProcessBuilder
+import java.util.Scanner
+
+import com.kirchnersolutions.pi.scala.rest.models.PsLine
 
 object ProcessService {
 
@@ -53,6 +56,95 @@ object ProcessService {
       }
     } catch {
       case err: Exception => "failed"
+    }
+  }
+
+  def killPiTemp(): String = {
+    try {
+      var found = false
+      var line = ""
+      val p = Runtime.getRuntime.exec("ps -aux")
+      val input = new BufferedReader(new InputStreamReader(p.getInputStream))
+      val in = new Scanner(input)
+      //line = input.readLine
+      while (in.hasNext()) {
+        if (line.contains("java -jar pitemp")) {
+          found = true
+        }
+        line = input.readLine
+      }
+      input.close()
+      if (found) {
+        "running"
+      } else {
+        Runtime.getRuntime.exec("bash startJ.sh")
+        "started"
+      }
+    } catch {
+      case err: Exception => "failed"
+    }
+  }
+
+  def getPID(process: String): Int = {
+    getProcesses().foreach(line => {
+      if(line.command.toLowerCase.contains(process.toLowerCase)){
+        return line.pid
+      }
+    })
+    return 0
+  }
+
+  def getPsLine(process: String): PsLine = {
+    getProcesses().foreach(line => {
+      if(line.command.toLowerCase.contains(process.toLowerCase)){
+        return line
+      }
+    })
+    return new PsLine("notfound", 0, 0, 0, 0, 0, "",, "", "", "", "")
+  }
+
+  def getProcesses(): Seq[PsLine] = {
+    val lines: Seq[PsLine] = new [PsLine]
+    try {
+      val p = Runtime.getRuntime.exec("ps -aux")
+      val input = new BufferedReader(new InputStreamReader(p.getInputStream))
+      val in = new Scanner(input)
+      var count: Int = 0
+      var user: String = ""
+      var pid: Int = 0
+      var cpu: Double = 0.0
+      var mem = 0.0
+      var vsz = 0
+      var rss = 0
+      var tty = "?"
+      var stat = ""
+      var start = ""
+      var time = ""
+      var command = ""
+      while (in.hasNext()) {
+        count match {
+          case 0 => user = in.next()
+          case 1 => pid = in.nextInt()
+          case 2 => cpu = in.nextDouble()
+          case 3 => mem = in.nextDouble()
+          case 4 => vsz = in.nextInt()
+          case 5 => rss = in.nextInt()
+          case 6 => tty = in.next()
+          case 7 => stat = in.next()
+          case 8 => start = in.next()
+          case 9 => time = in.next()
+          case 10 => {
+            command = in.next()
+            lines ++ Seq(new PsLine(user, pid, cpu, mem, vsz, rss, tty, stat, start, time, command))
+            count = -1
+          }
+        }
+        count += 1
+      }
+      input.close()
+      lines
+    } catch {
+      case err: Exception => Seq(new PsLine("failed", 0, 0, 0, 0, 0, "",, "", "", "", "")) ++ lines
     }
   }
 
